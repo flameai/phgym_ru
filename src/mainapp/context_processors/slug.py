1b5704cc4d36d83d3d8d45ac87c5abc818b2815c
Vladislav <xservking@gmail.com>
2018-01-10 12:33:41 +0500

    Fix slug for forms

--- src/mainapp/context_processors/slug.py
+++ src/mainapp/context_processors/slug.py
@@ -4,8 +4,8 @@ def slug(request):
     slug = request.path.split("/")[1]
     clubs = [(str(club.slug)) for club in Club.objects.all()]
     if slug not in clubs:
-        slug = None
-    #     club = Club.objects.get(order=0)
-    #     slug = str(club.slug)
+        # slug = None
+        club = Club.objects.get(order=0)
+        slug = str(club.slug)
 
     return {"slug": slug}
